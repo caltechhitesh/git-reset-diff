802c3bbc87e106af2a249959c6c32add1cbbef5e
hiteshvalkundakar <satyanarayanv52@gmail.com>
2026-05-28 16:32:07 +0530

    app.py

---
+++ app.py
@@ -0,0 +1,15 @@
+# Simple Python Program
+
+name = "Hitesh and suresh"
+age = 30
+
+print("My name is", name)
+print("My age is", age)
+
+# Function to add two numbers
+def add(a, b):
+    return a + b
+
+result = add(10, 20)
+
+print("Addition Result:", result)
